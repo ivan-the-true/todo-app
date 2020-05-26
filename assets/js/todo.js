41b1988686339bcf55a7e26ecfabe0f2b96f14be
Trevor Love <trevorlove777@gmail.com>
2020-05-26 16:32:01 -0400

    added logic to remove list items

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,10 @@
 $("li").on("click", function(){
   $(this).toggleClass("done");
 });
+
+$("span").on("click", function(event){
+  $(this).parent().fadeOut(300, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
