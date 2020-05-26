4bec07cefaa020ec73d2ebf0c6321c355b01849e
Trevor Love <trevorlove777@gmail.com>
2020-05-26 18:49:43 -0400

    add logic for adding new items. refactor logic for remove and strikeout so new items will be considered.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,10 +1,17 @@
-$("li").on("click", function(){
+$("#todoList").on("click", "li", function(){
   $(this).toggleClass("done");
 });
 
-$("span").on("click", function(event){
+$("#todoList").on("click", "span", function(event){
   $(this).parent().fadeOut(300, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("#newTodoInput").on("keypress", function(event){
+  if (event.which === 13) {
+    $("#todoList").append(`<li class=\"todo-item\"><span>X</span> ${$(this).val()}</li>`);
+    $(this).val("");
+  }
 });
